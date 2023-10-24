47ee3ce8ccbff7c4f98074d51e0fde265d5ef7a7
RidinRaccoon <mailforFilipe+github@gmail.com>
2023-10-24 02:42:27 +0100

    Author: [ Filipe Fonseca (@RidinRaccon) ] - Renamed folder: #2_New_Course_Content > #1_Course_Content - Renamed folder: #1_Old_Course_Content > #2_Old_Course_Content

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-/*
- * Old Course Content [ pre 23 October 2023]
- * [src > #1_Old_Course_Content]
- */
-// import { ExpensesApp } from './#1_Old_Course_Content/#1_Expense_App-Demo_Project/ExpensesApp';
-// import { InvestmentCalculatorApp } from './#1_Old_Course_Content/#2-Investment_Calculator/InvestmentCalculatorApp';
-import { AddUserApp } from './#1_Old_Course_Content/#3-Add_User_App/AddUserApp';
+/* Old Course Content [ pre 23 October 2023] | [src > #2_Old_Course_Content] */
+// import { ExpensesApp } from './#2_Old_Course_Content/#1_Expense_App-Demo_Project/ExpensesApp';
+// import { InvestmentCalculatorApp } from './#2_Old_Course_Content/#2-Investment_Calculator/InvestmentCalculatorApp';
+// import { AddUserApp } from './#2_Old_Course_Content/#3-Add_User_App/AddUserApp';
 
-/*
- * Current/New Course Content [23 October 2023]
- * [src > #1_Old_Course_Content]
- */
-// import { xxxApp } from './#2_New_Course_Content/#x-New_Course_Project/newProjectApp';
+/* Current/New Course Content [23 October 2023] | [src > #1_Course_Content] */
+// import { xxxApp } from './#1_Course_Content/#x-New_Course_Project/newProjectApp';
+// import { xxxApp } from './#1_Course_Content/#x-New_Course_Project/newProjectApp';
+// import { xxxApp } from './#1_Course_Content/#x-New_Course_Project/newProjectApp';
+import { AddUserApp } from './#2_Old_Course_Content/#3-Add_User_App/AddUserApp';
 
 const container: HTMLElement = document.getElementById('root')!;
 const root = createRoot(container);
 
-/*
- * Render Old Course Content
- */
+/* Render Course Content */
+root.render(<AddUserApp />);
+
+/* --------------------------*/
+/* Render Old Course Content */
 // root.render(<ExpensesApp />);
 // root.render(<InvestmentCalculatorApp />);
-root.render(<AddUserApp />);
+// root.render(<AddUserApp />);
