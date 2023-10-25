4680cc9589ed6cb8aa32f8dd77f9aa8a112d755a
RidinRaccoon <mailforFilipe+github@gmail.com>
2023-10-25 05:50:13 +0100

    Author: [ Filipe Fonseca (@RidinRaccoon) ]
    - Moved Core Concepts section in "ReactEssentialsApp" into a separate component [ CoreConceptList.tsx , CoreConceptList.css ]
    - Moved Examples section in "ReactEssentialsApp" into a separate component to avoid rerending the "ReactsEssentialApp" component when selecting a new example [ ExampleList.tsx , ExampleList.css ]
    - Fixed styles in ExamplesList's tab content to use the correct id tab-content [ ExampleList.css ]

--- src/#1_Course_Content/#1-React_Essentials-Demo_Project/ReactEssentialsApp.tsx
+++ src/#1_Course_Content/#1-React_Essentials-Demo_Project/ReactEssentialsApp.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Header } from './components/Header';
-import { CoreConcept } from './components/CoreConcept';
-import { TabButton } from './components/TabButton';
-import { CORE_CONCEPTS, EXAMPLES } from './assets/data/data';
+import { CoreConceptList } from './components/CoreConceptList';
+import { ExampleList } from './components/ExampleList';
+
 import './styles/index.css';
 
 /**
@@ -13,64 +13,12 @@ import './styles/index.css';
  ** The information listed (Core concepts and examples) can be changed in the data.tsx file
  */
 export function ReactEssentialsApp() {
-  // The selected topic in the examples section menu
-  const [selectedTopic, setselectedTopic] = useState<string>();
-
-  // Set the fallback text to be displayed in the examples section.
-  let tabContent = <p>Please select a topic.</p>;
-
-  // Set the tab content to match the selected topic in the menu buttons
-  if (selectedTopic && EXAMPLES[selectedTopic]) {
-    tabContent = (
-      <React.StrictMode>
-        <div id="tab-content">
-          <h3>{EXAMPLES[selectedTopic as keyof typeof EXAMPLES].title}</h3>
-          <p>{EXAMPLES[selectedTopic as keyof typeof EXAMPLES].description}</p>
-          <pre>
-            <code>{EXAMPLES[selectedTopic as keyof typeof EXAMPLES].code}</code>
-          </pre>
-        </div>
-      </React.StrictMode>
-    );
-  }
-
-  /** Handles the menu Toggle */
-  const handleSelect = (selectedButton: string) => {
-    setselectedTopic(selectedButton);
-  };
-
   return (
     <React.StrictMode>
       <Header />
       <main>
-        {/* Core Concepts section */}
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((data) => (
-              <CoreConcept
-                key={data.title}
-                title={data.title}
-                description={data.description}
-                src={data.src}
-                alt={data.alt}
-              />
-            ))}
-          </ul>
-        </section>
-
-        {/* Examples section */}
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            {Object.keys(EXAMPLES).map((key) => (
-              <TabButton key={key} active={selectedTopic === key} onSelect={() => handleSelect(key)}>
-                {EXAMPLES[key].title}
-              </TabButton>
-            ))}
-          </menu>
-          {tabContent}
-        </section>
+        <CoreConceptList />
+        <ExampleList />
       </main>
     </React.StrictMode>
   );
