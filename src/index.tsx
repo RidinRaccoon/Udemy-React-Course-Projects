6f4dcc2f5f32ac2fe4a1a234d7a681a07a51d479
RidinRaccoon <mailforFilipe+github@gmail.com>
2023-11-04 20:34:32 +0000

    Updated folder names and Structure Authors: [ Filipe Fonseca (@RidinRaccoon) ]
    - Renamed folders:
      - # 1-Course_Content --> Course_Content
      - # 2-Old_Course_Content --> Old_Course_Content
    
      - Old Course Content
        - # 1_Expense_App --> 1_Expense_App
        - # 2_Investment_Calculator --> 2_Investment_Calculator
        - # 3_Add_User_App --> 3_Add_User_App
    
      - Course Content
        - # 1-React_Essentials --> 1-React_Essentials
        - # 2-Almost_Final_Countdown --> 2-Almost_Final_Countadown
        - # 3-Place_Picker --> 3-Place_Picker
        - # 1-Tic-Tac-Toe --> 1-Tic-Tac-Toe
        - # 2-Investment_Calculator --> 2_Investment_Calculator
        - # 3-Project_Management --> 3_Project_Management
    
    - Created "Demo_Projects" and "Practice_Projects" folders under "Course_Content"
    - Added folders to "Demo_Projects":
      - 1-React_Essentials
      - 2-Almost_Final_Countadown
      - 3-Place_Picker
    
    - Added folders to "Practice_Projects":
      - 1-Tic-Tac-Toe
      - 2_Investment_Calculator
      - 3_Project_Management
    
    - Updated imports in "index.js" file

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-/* Old Course Content [ pre 23 October 2023] | [src > #2_Old_Course_Content] */
-// import { ExpensesApp } from './#2-Old_Course_Content/#1_Expense_App-Demo_Project/ExpensesApp';
-// import { InvestmentCalculatorApp } from './#2-Old_Course_Content/#2-Investment_Calculator/InvestmentCalculatorApp';
-// import { AddUserApp } from './#2-Old_Course_Content/#3-Add_User_App/AddUserApp';
+/* 
+* Old Course Content [ pre 23 October 2023] | [src > Old_Course_Content] 
+*/
+// import { ExpensesApp } from './Old_Course_Content/1_Expense_App-Demo_Project/ExpensesApp';
+// import { InvestmentCalculatorApp } from './Old_Course_Content/2-Investment_Calculator/InvestmentCalculatorApp';
+// import { AddUserApp } from './Old_Course_Content/3-Add_User_App/AddUserApp';
 
-/* Current/New Course Content [23 October 2023] | [src > #1_Course_Content] */
+/* 
+* Current/New Course Content [23 October 2023] | [src > Course_Content] 
+*/
 // DEMO PROJECTS
-// import { ReactEssentialsApp } from './#1-Course_Content/Demo_Projects/#1-React_Essentials/ReactEssentialsApp';
+// import { ReactEssentialsApp } from './Course_Content/Demo_Projects/1-React_Essentials/ReactEssentialsApp';
 
 // PRACTICE PROJECTS
-// import { TicTacToeApp } from './#1-Course_Content/Practice_Projects/#1-Tic-Tac-Toe/TicTacToeApp';
-// import { InvestmentCalculatorApp } from './#1-Course_Content/Practice_Projects/#2-Investment_Calculator/InvestmentCalculatorApp';
-import { ProjectManagementApp } from './#1-Course_Content/Practice_Projects/#3-Project_Management/ProjectManagementApp';
+// import { TicTacToeApp } from './Course_Content/Practice_Projects/1-Tic-Tac-Toe/TicTacToeApp';
+// import { InvestmentCalculatorApp } from './Course_Content/Practice_Projects/2-Investment_Calculator/InvestmentCalculatorApp';
+import { ProjectManagementApp } from './Course_Content/Practice_Projects/3-Project_Management/ProjectManagementApp';
 
 const container: HTMLElement = document.getElementById('root')!;
 const root = createRoot(container);
 
-/* Render Course Content */
 // DEMO PROJECTS
 // root.render(<ReactEssentialsApp />);
+
 // PRACTICE PROJECTS
 // root.render(<TicTacToeApp />);
 // root.render(<InvestmentCalculatorApp />);
